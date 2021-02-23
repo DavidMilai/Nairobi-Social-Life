3a08a3cf063f00620ddc79e42423637ecd6a9cd8
DavidMilai <davidmilai67@gmail.com>
2021-02-23 12:37:59 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/nairobi_social_life/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nairobi_social_life
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Config {
+  static final String appName = "NairobiSocialLife";
+  static final String apiUrl = "https://admin.solutechlabs.com/api";
+  static final ThemeData theme = ThemeData(
+    primarySwatch: Colors.purple,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+const double kMyPadding = 20;
+const Color kDefaultColor = Color(0xff5A005B);
